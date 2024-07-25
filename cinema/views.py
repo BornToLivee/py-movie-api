5249f4674828ce8a310d83be49c516161fc1d234
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-07-25 09:49:03 +0300

    fix imports and "PUT" in movie_details and set correct url

--- cinema/views.py
+++ cinema/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
+
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
@@ -26,7 +27,7 @@ def movie_details(request, pk):
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "PUT":
-        serializer = MovieSerializer(data=request.data)
+        serializer = MovieSerializer(movie, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
